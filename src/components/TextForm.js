1bd905f7e68f6cec2eb08c74906482847d74c582
gaurav147-star <gauravgupta9158@gmail.com>
2021-12-09 19:54:29 +0530

    added download button and readme modified

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,6 +28,16 @@ export default function TextForm(props) {
     const newText = "";
     setText(newText);
   };
+  const downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([document.getElementById('box').value],    
+                {type: 'text/plain;charset=utf-8'});
+    element.href = URL.createObjectURL(file);
+    element.download = "myFile.txt";
+    document.body.appendChild(element);
+    element.click();
+  }
+
   const [text, setText] = useState("");
   //   const text1 = setText(text);
 
@@ -50,22 +60,25 @@ export default function TextForm(props) {
           rows="9"
           placeholder="Enter text here"
         ></textarea>
-        <button className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={ConvertToUp}>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={ConvertToUp}>
           ChangeToUpperCase
         </button>
-        <button className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={ConvertToLo}>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={ConvertToLo}>
           ChangeToLowerCase
         </button>
-        <button className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={Clear}>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={Clear}>
           Clear
         </button>
 
-        <button className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={CopyText}>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={CopyText}>
           Copy Text
         </button>
 
-        <button className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={ExtraSpace}>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={ExtraSpace}>
           Remove Extra Space
+        </button>
+        <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1 shadow p-2 rounded" onClick={downloadTxtFile}>
+          Download text
         </button>
       </div>
 
@@ -82,19 +95,30 @@ export default function TextForm(props) {
         <h2>
           {" "}
           <b> Your summary is here </b>
-        </h2>
+          </h2>
         <p>
           {" "}
-          {text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters
+          {
+            text.split(/\s+/).filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
         </p>
-        <p> {0.08 * text.split(" ").length} minutes read</p>
+        <p>
+          {" "}
+          {0.08 *
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length}{" "}
+          minutes read
+        </p>
 
         <h3>
           <b> Preview</b>{" "}
         </h3>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
       </div>
-      
     </>
   );
 }
